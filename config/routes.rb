6123dffd011bcc27ce1b26393db9354c5bff45a9
SAAHIL SABU HAMEED <saahil.sabu@gmail.com>
2024-01-04 00:07:28 +0530

    route: create route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  get "blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "blog_posts/:id", to: "blog_posts#show" ,as: :blog_post
+  post "blog_posts", to: "blog_posts#create", as: :blog_posts
   root "blog_posts#index"
 end
