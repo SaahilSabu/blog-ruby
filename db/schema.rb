dd03dcf2059c36bec8064bfb1ee6b4dfe7970fe9
SAAHIL SABU HAMEED <saahil.sabu@gmail.com>
2024-01-04 16:57:54 +0530

    model changes for scheduled , draft and publish posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_04_061934) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_04_095929) do
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
